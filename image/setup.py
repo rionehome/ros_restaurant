da7b62545719b07b1c7d7cad24563d1cf50d38cc
yuuka <yuka.y35.0107@ezweb.ne.jp>
2019-10-23 19:48:10 +0900

    soundとimageのsetup.cfg, setup.py, package.xml, controlのCMakeLists.txt, package.xmlを書いたよ.

---
+++ image/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+
+package_name = 'image'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[],
+    py_modules=[
+        "talker",
+        "listener"
+    ],
+    data_files=[
+        ('share/' + package_name, ['package.xml', 'test.launch.py']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author='MATSUDAYamato', 'YAMAMOTOYuuka',
+    author_email='is0476hv@ed.ritsumei.ac.jp', 'rr0113ke@ed.ritsumei.ac.jp',
+    maintainer='MATSUDAYamato',
+    maintainer_email='is0476hv@ed.ritsumei.ac.jp',
+    keywords=['ROS'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description='Examples of minimal publishers using rospy.',
+    license='Apache License, Version 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'talker=talker:main',
+            'listener=listener:main',
+        ],
+    },
+)
